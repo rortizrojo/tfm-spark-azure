fd69ac9126987ff642406fa929cfb3f27596ff67
roberto.ortiz.rojo.contractor <roberto.ortiz.rojo.contractor@bbva.com>
2019-07-09 09:50:11 +0200

    Añadido Spark a las dependencias del pom y lectura del csv de entrada

--- src/main/scala/tfm/App.scala
+++ src/main/scala/tfm/App.scala
@@ -1,5 +1,7 @@
 package tfm
 
+import org.apache.spark.sql.SparkSession
+
 /**
  * @author ${user.name}
  */
@@ -10,6 +12,30 @@ object App {
   def main(args : Array[String]) {
     println( "Hello World!" )
     println("concat arguments = " + foo(args))
+    val spark = SparkSession
+      .builder()
+      .master("local[*]")
+      .getOrCreate()
+
+
+    val csv = spark.read
+        .option("header", true)
+      .option("delimiter", ";")
+      .csv("MuestraDatos.csv")
+
+    csv.show()
+
+
+
+
+
+
   }
+
+
+
+
+
+
 
 }
