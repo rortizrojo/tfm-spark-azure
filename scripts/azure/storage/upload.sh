d4a12968a0585d68e314e00d04508d7675ba39b7
rortizrojo <rortizrojo@gmail.com>
2019-07-23 09:19:11 +0200

    Prueba de subida de fichero

---
+++ scripts/azure/storage/upload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# A simple Azure Storage example script
+
+export AZURE_STORAGE_ACCOUNT=cuentaalmacenamientotfm
+export AZURE_STORAGE_KEY=<storage_account_key>
+
+export container_name=contenedor_datos
+export blob_name=nombre_blob
+export file_to_upload=MuestraDatos.csv
+export destination_file=muestraSubido.csv
+
+echo "Creating the container..."
+az storage container create --name $container_name
+
+echo "Uploading the file..."
+az storage blob upload --container-name $container_name --file $file_to_upload --name $blob_name
+
+echo "Listing the blobs..."
+az storage blob list --container-name $container_name --output table
+
+echo "Downloading the file..."
+az storage blob download --container-name $container_name --name $blob_name --file $destination_file --output table
+
+echo "Done"
