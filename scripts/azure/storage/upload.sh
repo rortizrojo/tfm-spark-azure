12a544f0edbe54cc654a42e40aa6bb79bc211254
rortizrojo <rortizrojo@gmail.com>
2019-07-23 14:55:59 +0200

    Corregida la creación del contenedor en ADLSG2

--- scripts/azure/storage/upload.sh
+++ scripts/azure/storage/upload.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 # A simple Azure Storage example script
 
-export AZURE_STORAGE_ACCOUNT=$0
-export AZURE_STORAGE_KEY=$1
+export AZURE_STORAGE_ACCOUNT=$1
+export AZURE_STORAGE_KEY=$2
 
-export container_name=contenedor_datos
+export container_name=contenedordatos1
 export blob_name=nombre_blob
 export file_to_upload=MuestraDatos.csv
 export destination_file=muestraSubido.csv
 
-echo "Creating the container..."
-az storage container create --name $container_name
 
-echo "Uploading the file..."
-az storage blob upload --container-name $container_name --file $file_to_upload --name $blob_name
+sas_token=$(az storage account generate-sas --account-key $AZURE_STORAGE_KEY --account-name $AZURE_STORAGE_ACCOUNT --expiry 2020-01-01 --https-only --permissions acuw --resource-types co --services bfqt)
 
-echo "Listing the blobs..."
-az storage blob list --container-name $container_name --output table
+echo "Creating the container...$container_name"
+url=https://$AZURE_STORAGE_ACCOUNT.dfs.core.windows.net/$container_name?$sas_token
 
-echo "Downloading the file..."
-az storage blob download --container-name $container_name --name $blob_name --file $destination_file --output table
+url="${url//\"/}"
+echo $url
+./azcopy make $url
 
-echo "Done"
+
+
+#
+#az storage container create --name $container_name
+#
+#echo "Uploading the file..."
+#az storage blob upload --container-name $container_name --file $file_to_upload --name $blob_name
+#
+#echo "Listing the blobs..."
+#az storage blob list --container-name $container_name --output table
+#
+#echo "Downloading the file..."
+#az storage blob download --container-name $container_name --name $blob_name --file $destination_file --output table
+#
+#echo "Done"
