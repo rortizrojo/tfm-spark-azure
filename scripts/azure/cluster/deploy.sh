350f8e63c87997ce02d9d90c21d205478a2d9f85
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-07-29 20:17:47 +0200

    Arreglado script bash

--- scripts/azure/cluster/deploy.sh
+++ scripts/azure/cluster/deploy.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-
 # -e: immediately exit if any command has a non-zero exit status
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
