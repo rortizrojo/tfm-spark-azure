2b5130468d33b8e4ba49b798bbccdf3505fd8ed6
rortizrojo <rortizrojo@gmail.com>
2019-07-30 13:44:04 +0200

    Corregido error en parseo de argumentos de python

--- scripts/azure/modifyStorageParameters.py
+++ scripts/azure/modifyStorageParameters.py
@@ -5,7 +5,7 @@ def modify(args):
 
     print(args)
 
-    with open(args.storageName, 'r+') as f:
+    with open(args.storageFile, 'r+') as f:
         data = json.load(f)
         data['parameters']['storageAccountName']['value']=args.storageName # <--- add `id` value.
 
@@ -15,7 +15,7 @@ def modify(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-sf', '--file')
+    parser.add_argument('-sf', '--storageFile')
     parser.add_argument('-sn', '--storageName')
 
     #parser.add_argument('-v', dest='verbose', action='store_true')
