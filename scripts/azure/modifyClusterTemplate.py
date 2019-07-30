7185dae33e4a5c11ed5c42e937b7465bd4805576
rortizrojo <rortizrojo@gmail.com>
2019-07-30 13:35:55 +0200

    Añadido la modificación de las plantillas json

---
+++ scripts/azure/modifyClusterTemplate.py
@@ -0,0 +1,42 @@
+import json
+
+import argparse
+def modify(args):
+
+    print(args)
+
+    with open(args.clusterFile, 'r+') as f:
+        data = json.load(f)
+
+
+        # Cluster identity parameters
+        data['resources'][0]['identity']['type']=args.identity # <--- add `id` value.
+        data['resources'][0]['identity']['userAssignedIdentities']={ args.userAssignedIdentities: {}}
+
+
+        # Cluster storage Options
+        data['resources'][0]['properties']['storageProfile']['storageaccounts'][0]['name']=args.storageName + ".dfs.core.windows.net"
+        data['resources'][0]['properties']['storageProfile']['storageaccounts'][0]['resourceId']=args.storageId
+        data['resources'][0]['properties']['storageProfile']['storageaccounts'][0]['msiResourceId']=args.userAssignedIdentities
+
+
+        f.seek(0)        # <--- should reset file position to the beginning.
+        json.dump(data, f, indent=4)
+        f.truncate()     # remove remaining part
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-cf', '--clusterFile')
+    parser.add_argument('-i', '--identity')
+    parser.add_argument('-ua', '--userAssignedIdentities')
+    parser.add_argument('-si', '--storageId')
+    parser.add_argument('-sn', '--storageName')
+
+    #parser.add_argument('-v', dest='verbose', action='store_true')
+    args = parser.parse_args()
+
+    modify(args)
---
+++ scripts/azure/modifyStorageParameters.py
@@ -0,0 +1,24 @@
+import json
+
+import argparse
+def modify(args):
+
+    print(args)
+
+    with open(args.storageName, 'r+') as f:
+        data = json.load(f)
+        data['parameters']['storageAccountName']['value']=args.storageName # <--- add `id` value.
+
+        f.seek(0)        # <--- should reset file position to the beginning.
+        json.dump(data, f, indent=4)
+        f.truncate()     # remove remaining part
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file')
+    parser.add_argument('-sn', '--storageName')
+
+    #parser.add_argument('-v', dest='verbose', action='store_true')
+    args = parser.parse_args()
+
+    modify(args)
