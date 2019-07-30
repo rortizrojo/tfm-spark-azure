9a8bda847f133134c588fd08fa0efebca250e52e
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-07-30 23:03:01 +0200

    Añadida parametrización en el nombre de cluster en el script

--- scripts/azure/modifyClusterTemplate.py
+++ scripts/azure/modifyClusterTemplate.py
@@ -5,7 +5,7 @@ def modify(args):
 
     print(args)
 
-    with open(args.clusterFile, 'r+') as f:
+    with open(args.clusterTemplateFile, 'r+') as f:
         data = json.load(f)
 
 
@@ -25,16 +25,27 @@ def modify(args):
         f.truncate()     # remove remaining part
 
 
+    with open(args.clusterParametersFile, 'r+') as f:
+        data = json.load(f)
 
+        # Cluster identity parameters
+        data['parameters']['clusterName']['value']=args.name
+
+
+        f.seek(0)        # <--- should reset file position to the beginning.
+        json.dump(data, f, indent=4)
+        f.truncate()     # remove remaining part
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-cf', '--clusterFile')
+    parser.add_argument('-ctf', '--clusterTemplateFile')
+    parser.add_argument('-cpf', '--clusterParametersFile')
     parser.add_argument('-uai', '--userAssignedIdentities')
     parser.add_argument('-si', '--storageId')
     parser.add_argument('-sn', '--storageName')
     parser.add_argument('-c', '--container')
+    parser.add_argument('-n', '--name')
 
     #parser.add_argument('-v', dest='verbose', action='store_true')
     args = parser.parse_args()
