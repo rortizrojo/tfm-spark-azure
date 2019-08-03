8c054c76427027dc91fe088a85db869dee5fe0e6
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-08-03 13:30:13 +0200

    Pruebas con ssh

--- scripts/azure/testSSH.sh
+++ scripts/azure/testSSH.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 cluster_info=`az hdinsight list --resource-group  grupoRecursosTfm`
 cluster_name=`echo $cluster_info | jq -r ".[0].name"`
 sshUser=`echo $cluster_info | jq -r ".[0].properties.computeProfile.roles[0].osProfile.linuxOperatingSystemProfile.username"`
@@ -9,7 +8,6 @@ contenedor=`echo $cluster_info | jq -r ".[0].properties.storageProfile.storageac
 DATA_LAKE_MAIN_PATH=abfs://\${contenedor}@\${pathAccount}/
 sshHostName=\${sshUser}@\${cluster_name}-ssh.azurehdinsight.net
 sshpass -p 'tfmPassword.2019' ssh \$sshHostName -o StrictHostKeyChecking=no
-
 
 hdfs dfs -ls \$DATA_LAKE_MAIN_PATH
 exit
