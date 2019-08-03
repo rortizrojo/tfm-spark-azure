8bcd55b794b7dc133b65694cb4d1f8665ee799c6
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-08-03 13:32:27 +0200

    Pruebas con ssh

--- scripts/azure/testSSH.sh
+++ scripts/azure/testSSH.sh
@@ -5,9 +5,9 @@ cluster_name=`echo $cluster_info | jq -r ".[0].name"`
 sshUser=`echo $cluster_info | jq -r ".[0].properties.computeProfile.roles[0].osProfile.linuxOperatingSystemProfile.username"`
 pathAccount=`echo $cluster_info | jq -r ".[0].properties.storageProfile.storageaccounts[0].name"`
 contenedor=`echo $cluster_info | jq -r ".[0].properties.storageProfile.storageaccounts[0].fileSystem"`
-DATA_LAKE_MAIN_PATH=abfs://\${contenedor}@\${pathAccount}/
-sshHostName=\${sshUser}@\${cluster_name}-ssh.azurehdinsight.net
-sshpass -p 'tfmPassword.2019' ssh \$sshHostName -o StrictHostKeyChecking=no
+DATA_LAKE_MAIN_PATH=abfs://${contenedor}@${pathAccount}/
+sshHostName=${sshUser}@${cluster_name}-ssh.azurehdinsight.net
+sshpass -p 'tfmPassword.2019' ssh $sshHostName -o StrictHostKeyChecking=no
 
 hdfs dfs -ls \$DATA_LAKE_MAIN_PATH
 exit
