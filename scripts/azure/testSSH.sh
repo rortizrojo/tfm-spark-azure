fd6922a56191a4174dae63c58d5688f637c9cd8a
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-08-03 15:24:29 +0200

    Pruebas con ssh

--- scripts/azure/testSSH.sh
+++ scripts/azure/testSSH.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
+ficheroInput="muestraSubido.csv"
+outputPath="/testing/fichero.csv"
+
+
 cluster_info=`az hdinsight list --resource-group  grupoRecursosTfm`
 cluster_name=`echo $cluster_info | jq -r ".[0].name"`
 sshUser=`echo $cluster_info | jq -r ".[0].properties.computeProfile.roles[0].osProfile.linuxOperatingSystemProfile.username"`
 pathAccount=`echo $cluster_info | jq -r ".[0].properties.storageProfile.storageaccounts[0].name"`
-contenedor=`echo $cluster_info | jq -r ".[0].properties.storageProfile.storageaccounts[0].fileSystem"`
-DATA_LAKE_MAIN_PATH=abfs://${contenedor}@${pathAccount}/
+DATA_LAKE_MAIN_PATH=abfs://contenedoralmacenamiento@${pathAccount}/
 sshHostName=${sshUser}@${cluster_name}-ssh.azurehdinsight.net
-test="hdfs dfs -ls $DATA_LAKE_MAIN_PATH > ficheroSalida"
+
+command="hdfs dfs -ls $DATA_LAKE_MAIN_PATH/$ficheroInput /$outputPath"
 echo $test
 echo "SSH HOST: $sshHostName"
-RESULTS=$(sshpass -v -p 'tfmPassword.2019' ssh -tt $sshHostName -o StrictHostKeyChecking=no 'mkdir carpetaTest;$test')
-echo "Resultados: $RESULTS"
+sshpass -p 'tfmPassword.2019' ssh -tt $sshHostName -o StrictHostKeyChecking=no "$command"
 
 
 exit
