3b60310de410b200c2f4ce9345b6a456abc38a46
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-08-10 19:45:18 +0200

    Modificado el código Python

--- scripts/python/prueba.py
+++ scripts/python/prueba.py
@@ -1,4 +1,10 @@
+from pyspark.sql import SparkSession
 
 if __name__== "__main__":
-    import utils
+
+    spark = SparkSession.builder \
+        .appName("Separador Columnas") \
+        .getOrCreate()
+    spark.sparkContext.addPyFile("my_arch.zip")
+    import utils.py
     utils.data_splitter()
