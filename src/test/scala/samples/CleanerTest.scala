8cf3054dc05f629379128b6e28099176757a86ec
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-08-15 00:25:17 +0200

    Test modificaciones tests

--- src/test/scala/samples/CleanerTest.scala
+++ src/test/scala/samples/CleanerTest.scala
@@ -7,24 +7,28 @@ import org.scalatest.FunSuite
 @Test
 class CleanerTest extends FunSuite with DataFrameSuiteBase {
 
+  @Test
+  def testCleaner(): Unit ={
+    test("test") {
+      val sqlCtx = sqlContext
+      import sqlCtx.implicits._
 
-  test("test") {
-    val sqlCtx = sqlContext
-    import sqlCtx.implicits._
+      val input1 = sc.parallelize(List(1, 2, 3)).toDF
+      assertDataFrameEquals(input1, input1) // equal
 
-    val input1 = sc.parallelize(List(1, 2, 3)).toDF
-    assertDataFrameEquals(input1, input1) // equal
+      val input2 = sc.parallelize(List(4, 5, 6)).toDF
 
-    val input2 = sc.parallelize(List(4, 5, 6)).toDF
+      val input3 = sc.parallelize(List(4, 5, 6)).toDF
 
-    val input3 = sc.parallelize(List(4, 5, 6)).toDF
+      intercept[org.scalatest.exceptions.TestFailedException] {
+        assertDataFrameEquals(input1, input2) // not equal
+      }
 
-    intercept[org.scalatest.exceptions.TestFailedException] {
-      assertDataFrameEquals(input1, input2) // not equal
+
+      assertDataFrameEquals(input2, input3) // not equal
+
     }
-
-
-    assertDataFrameEquals(input2, input3) // not equal
-
   }
+
+
 }
