12945e3ea6069f415264d7b0623b07d49458b747
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-08-25 14:23:11 +0200

    Probando scripts

--- scripts/tasks/spark-submit.sh
+++ scripts/tasks/spark-submit.sh
@@ -18,19 +18,20 @@ echo "Usuario actual : $USER"
 
 sudo chown -v $jenkins ~/.ssh/known_hosts
 sudo chown -v $jenkins /home/rortizrojo/.ssh/known_hosts
+
+echo "Ejecutando ssh-keygen rortizrojo"
 ssh-keygen -f "/home/rortizrojo/.ssh/known_hosts" -R "cluster-tfm-ssh.azurehdinsight.net"
-
-
-echo "Comanddo spark-submit: $commandExecuteSparkSubmit"
-echo "Ejecutando ssh-keygen"
-
+echo "Ejecutando ssh-keygen jenkins"
 ssh-keygen -f "/var/lib/jenkins/.ssh/known_hosts" -R "${cluster_name}-ssh.azurehdinsight.net"
+
+
 # Copia de fichero jar a cluster
 echo "Subiendo jar al cluster"
 sshpass -p 'tfmPassword.2019' scp -o StrictHostKeyChecking=no target/tfmSpark-1.0-SNAPSHOT.jar $sshHostName:cleaning_lib.jar
 #Ejecución de comandos
 echo "Ejecutando comando: $command"
 echo "Ejecutando comando: $sshHostName"
+echo "Ejecutando spark-submit: $commandExecuteSparkSubmit"
 sshpass -p 'tfmPassword.2019' ssh -tt $sshHostName -o StrictHostKeyChecking=no "$command;$commandExecuteSparkSubmit"
 
 exit
