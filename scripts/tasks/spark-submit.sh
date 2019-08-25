62c6d69ddc7045c4cce3c4666f99b760c1e21206
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-08-25 13:35:08 +0200

    Probando scripts

--- scripts/tasks/spark-submit.sh
+++ scripts/tasks/spark-submit.sh
@@ -13,18 +13,18 @@ sshHostName=${sshUser}@${cluster_name}-ssh.azurehdinsight.net
 # Declaración de comandos a ejecutar en cluster
 command="hdfs dfs -cp $DATA_LAKE_MAIN_PATH$ficheroInput $outputPath"
 commandExecuteSparkSubmit="spark-submit --conf spark.yarn.maxAppAttempts=1 --master yarn --deploy-mode cluster --class tfm.Main cleaning_lib.jar"
-ssh-keygen -f "/home/rortizrojo/.ssh/known_hosts" -R "cluster-tfm-ssh.azurehdinsight.net"
+#ssh-keygen -f "/home/rortizrojo/.ssh/known_hosts" -R "cluster-tfm-ssh.azurehdinsight.net"
 
 
 echo "Comanddo spark-submit: $commandExecuteSparkSubmit"
 echo "Ejecutando ssh-keygen"
-sudo ssh-keygen -t -f "/var/lib/jenkins/.ssh/known_hosts" -R "${cluster_name}-ssh.azurehdinsight.net"
+ssh-keygen -f "/var/lib/jenkins/.ssh/known_hosts" -R "${cluster_name}-ssh.azurehdinsight.net"
 # Copia de fichero jar a cluster
 echo "Subiendo jar al cluster"
-sudo sshpass -p 'tfmPassword.2019' scp -t -o StrictHostKeyChecking=no target/tfmSpark-1.0-SNAPSHOT.jar $sshHostName:cleaning_lib.jar
+sshpass -p 'tfmPassword.2019' scp -o StrictHostKeyChecking=no target/tfmSpark-1.0-SNAPSHOT.jar $sshHostName:cleaning_lib.jar
 #Ejecución de comandos
 echo "Ejecutando comando: $command"
 echo "Ejecutando comando: $sshHostName"
-sudo sshpass -p 'tfmPassword.2019' ssh -tt $sshHostName -o StrictHostKeyChecking=no "$command;$commandExecuteSparkSubmit"
+sshpass -p 'tfmPassword.2019' ssh -tt $sshHostName -o StrictHostKeyChecking=no "$command;$commandExecuteSparkSubmit"
 
 exit
