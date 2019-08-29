b6386c987dd38acb80e7aa11a0b4c2683960d814
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-08-29 22:07:10 +0200

    Añadida parametrización del tamaño del cluster en el pipeline de Jenkins

--- scripts/azure/modifyClusterTemplate.py
+++ scripts/azure/modifyClusterTemplate.py
@@ -19,6 +19,14 @@ def modify(args):
         data['resources'][0]['properties']['storageProfile']['storageaccounts'][0]['msiResourceId']=args.userAssignedIdentities
         data['resources'][0]['properties']['storageProfile']['storageaccounts'][0]['fileSystem']=args.container
 
+        # Cluster size options
+        # Head Nodes
+        data['resources'][0]['properties']['computeProfile']['roles'][0]['targetInstanceCount']=int(args.head_node_instances)
+        data['resources'][0]['properties']['computeProfile']['roles'][0]['hardwareProfile']['vmSize']=args.head_node_type
+        # Worker Nodes
+        data['resources'][0]['properties']['computeProfile']['roles'][1]['targetInstanceCount']=int(args.worker_node_instances)
+        data['resources'][0]['properties']['computeProfile']['roles'][1]['hardwareProfile']['vmSize']=args.worker_node_type
+
 
         f.seek(0)        # <--- should reset file position to the beginning.
         json.dump(data, f, indent=4)
@@ -30,7 +38,8 @@ def modify(args):
 
         # Cluster identity parameters
         data['parameters']['clusterName']['value']=args.name
-
+        # Cluster worker node number
+        data['parameters']['clusterWorkerNodeCount']['value']=int(args.worker_node_instances)
 
         f.seek(0)        # <--- should reset file position to the beginning.
         json.dump(data, f, indent=4)
@@ -46,6 +55,10 @@ if __name__ == '__main__':
     parser.add_argument('-sn', '--storageName')
     parser.add_argument('-c', '--container')
     parser.add_argument('-n', '--name')
+    parser.add_argument('-hnt', '--head_node_type')
+    parser.add_argument('-wnt', '--worker_node_type')
+    parser.add_argument('-hni', '--head_node_instances')
+    parser.add_argument('-wni', '--worker_node_instances')
 
     #parser.add_argument('-v', dest='verbose', action='store_true')
     args = parser.parse_args()
