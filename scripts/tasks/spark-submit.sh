226a2106833888daa2cbd08e36718a2640c8d71f
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-09-08 22:57:15 +0200

    Corregido error en script spark submit

--- scripts/tasks/spark-submit.sh
+++ scripts/tasks/spark-submit.sh
@@ -7,7 +7,7 @@ cluster_info=`az hdinsight list --resource-group  grupoRecursosTfm`
 cluster_name=`echo $cluster_info | jq -r ".[0].name"`
 sshUser=`echo $cluster_info | jq -r ".[0].properties.computeProfile.roles[0].osProfile.linuxOperatingSystemProfile.username"`
 pathAccount=`echo $cluster_info | jq -r ".[0].properties.storageProfile.storageaccounts[0].name"`
-container=`echo $cluster_info | jq -r ".[0].properties.storageProfile.storageaccounts[0].fileSystem`
+container=`echo $cluster_info | jq -r ".[0].properties.storageProfile.storageaccounts[0].fileSystem"`
 DATA_LAKE_MAIN_PATH=abfs://${container}@${pathAccount}/
 sshHostName=${sshUser}@${cluster_name}-ssh.azurehdinsight.net
 
