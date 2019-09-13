496fb2794b42108e8d66944228d79c32e6ee6b7a
Roberto Ortiz Rojo <rortizrojo@gmail.com>
2019-09-13 22:48:11 +0200

    Eliminado el muestreado para leer el fichero completo

--- src/main/scala/tfm/Main.scala
+++ src/main/scala/tfm/Main.scala
@@ -30,7 +30,7 @@ object Main {
     //val dfInput = getInputData(args(0), "\t")
     val dfInput = getInputData(args(0), args(1))
         .select("ID", categoria, columnQuery, columnKeyword)
-        .sample(0.3)
+        //.sample(0.3)
 
 
     logger.warn("Filas leídas: " + dfInput.count())
